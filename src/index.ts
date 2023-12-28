755d18a1c392e75e2acb32b7157cdd2114d9aa01
Marcio Moreno <marcio@mobr.ai>
2023-12-28 20:31:25 +0100

    supporting sparql queries using rdflib

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
-import { Canister, query, text, update, Void } from 'azle';
+import { Canister, query, text, update, Void } from 'azle'
+import { initStore, queryStore } from './triplestore.js'
 
 // This is a global variable that is stored on the heap
-let message = '';
+let iconto = ''
 
 export default Canister({
     // Query calls complete quickly because they do not go through consensus
-    getMessage: query([], text, () => {
-        return message;
+    getOntology: query([], text, () => {
+        return iconto
     }),
     // Update calls take a few seconds to complete
     // This is because they persist state changes and go through consensus
-    setMessage: update([text], Void, (newMessage) => {
-        message = newMessage; // This change will be persisted
+    setOntology: update([text], Void, (newOntology) => {
+        iconto = newOntology
+        initStore(iconto)
+    }),
+    sparqlQuery: query([text], text, (strSparql) => {
+        const bindings = queryStore(strSparql)
+        console.log(bindings)
+        return bindings
     })
-});
+})
+
+// PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX ponto: <http://www.mobr.ai/ontologies/ponto#> PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> SELECT ?s WHERE { ?s ?p ?o . }
---
+++ src/triplestore.js
@@ -0,0 +1,91 @@
+import { Store, parse, SPARQLToQuery } from 'rdflib'
+
+var tStore
+
+function initStore(iconto) {
+    tStore = new Store()
+    var uri = 'http://www.mobr.ai/ontologies/ponto#'
+    var mimeType = 'text/turtle'
+
+    try {
+        parse(iconto, tStore, uri, mimeType)
+        console.log("parse done")
+
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+//function match() {
+    // tStore.query(query, result => {
+    //     console.log("result")
+    //     console.log(result["?s"].value)
+    // })
+    // const allTriples = tStore.statementsMatching(undefined, undefined, undefined);
+    // allTriples.forEach(function(triple) {
+    //     if(triple.object.termType === "NamedNode") {
+    //         console.log('<' + triple.object.uri) + '>';
+    //     }
+    //     else  {
+    //         console.log('\'' + triple.object.value + '\'');
+    //     }
+    // })
+//}
+
+
+function queryStore(strSparql) {
+    if (tStore.length == 0) {
+        console.log("triplestore is empty")
+        return ""
+    }
+    console.log("triplestore size is")
+    console.log(tStore.length)
+
+    try {
+        const query = SPARQLToQuery(strSparql, null, tStore)
+        if (query === false) {
+            console.log("invalid query")
+            return "invalid sparql query"
+        }
+        console.log(query)
+        var vArr = []
+        const vars = query["vars"]
+        for (var i=0; i < vars.length; i++) {
+            vArr.push("?" + vars[i]["value"])
+        }
+
+        if (!vArr.length) {
+            return ""
+        }
+
+        const res = tStore.querySync(query)
+        var strRes = "{"
+        for (var i=0; i < res.length; i++) {
+            if (strRes != "{") {
+                strRes = strRes + ", "
+            }
+
+            var strRow = ""
+            for (var j=0; j < vArr.length; j++) {
+                if (res[i].hasOwnProperty(vArr[j])) {
+                    const key = vArr[j]
+                    if (strRow != "") {
+                        strRow = strRow + ", "
+                    }
+                    strRow = strRow + '"' + key + '": "' + res[i][key]["value"] + '"'    
+                }
+            }            
+            if (strRow != "") {
+                strRes = strRes + i.toString() + ": {" + strRow + "}"
+            }    
+        }
+        return strRes + "}"
+
+    } catch (err) {
+        console.log(err)
+    }
+    return ""
+}
+
+export {initStore, queryStore}
